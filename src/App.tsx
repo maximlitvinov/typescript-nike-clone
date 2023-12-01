731e8397a2eb0ee72f986d9e7adce4360a6649de
Maxim <maximlitvinov.2611@gmail.com>
2023-12-01 13:08:17 +0300

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Hero, PopularProducts, Subscribe, SpecialOffer, SuperQuality, CustomerReviews, Footer, Services } from "./sections";
+
+const App = () => {
+  return (
+    <main className="text-center">
+      Nav
+      <section className="my-2">Hero</section>
+      <section className="my-24">PopularProducts</section>
+      <section className="my-24">SuperQuality</section>
+      <section className="my-24">Services</section>
+      <section className="my-24">SpecialOffer</section>
+      <section className="my-24">CustomerReviews</section>
+      <section className="my-24">Subscribe</section>
+      <section className="my-24">Footer</section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p className="text-center font-bold">Header</p>
+    </div>
+  );
+};
+
+export default Header;
